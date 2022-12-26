8fb74ee6d1834ff99e3c356f328e897afe2622e4
WorstNormal <vladislavgaev84037@gmail.com>
2022-12-26 23:45:09 +0700

    26.12.22

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sqlite3
 class Window(QWidget):
 	def __init__(self):
 		self.con = sqlite3.connect("database.sqlite")
+		self.cur = self.con.cursor()
 		super().__init__()
 		self.main_creat()
 		self.make_create()
@@ -75,14 +76,14 @@ class Window(QWidget):
 	def create_create(self):
 		# Кнопки
 		self.btn_next_create = QPushButton('Продолжить', self)
-		self.btn_next_create.setGeometry(1000, 800, 200, 100)
+		self.btn_next_create.setGeometry(860, 650, 200, 100)
 		self.btn_next_create.setStyleSheet(pushButton_StyleSheet)
 		self.btn_next_create.setObjectName("pushButton")
 		self.btn_next_create.clicked.connect(self.create_next)
 		self.btn_next_create.hide()
 		
 		self.btn_back_create = QPushButton('Назад', self)
-		self.btn_back_create.setGeometry(720, 800, 200, 100)
+		self.btn_back_create.setGeometry(860, 800, 200, 100)
 		self.btn_back_create.setStyleSheet(pushButton_StyleSheet)
 		self.btn_back_create.setObjectName("pushButton")
 		self.btn_back_create.clicked.connect(self.create_back)
@@ -101,31 +102,31 @@ class Window(QWidget):
 		self.input_coun_create.setStyleSheet(input_StyleSheet)
 		
 		self.input_var1_create = QLineEdit(self)
-		self.input_var1_create.resize(35, 50)
-		self.input_var1_create.move(1100, 500)
+		self.input_var1_create.resize(300, 50)
+		self.input_var1_create.move(1100, 250)
 		self.input_var1_create.hide()
 		self.input_var1_create.setStyleSheet(input_StyleSheet)
 		
 		self.input_var2_create = QLineEdit(self)
-		self.input_var2_create.resize(35, 50)
-		self.input_var2_create.move(1100, 500)
+		self.input_var2_create.resize(300, 50)
+		self.input_var2_create.move(1100, 350)
 		self.input_var2_create.hide()
 		self.input_var2_create.setStyleSheet(input_StyleSheet)
 		
 		self.input_var3_create = QLineEdit(self)
-		self.input_var3_create.resize(35, 50)
-		self.input_var3_create.move(1100, 500)
+		self.input_var3_create.resize(300, 50)
+		self.input_var3_create.move(1100, 450)
 		self.input_var3_create.hide()
 		self.input_var3_create.setStyleSheet(input_StyleSheet)
 		
 		self.input_var4_create = QLineEdit(self)
-		self.input_var4_create.resize(35, 50)
-		self.input_var4_create.move(1100, 500)
+		self.input_var4_create.resize(300, 50)
+		self.input_var4_create.move(1100, 550)
 		self.input_var4_create.hide()
 		self.input_var4_create.setStyleSheet(input_StyleSheet)
 		# Переменные
-		self.number_ind_create = 0
-		self.number_coun_create = 0
+		self.int_ind_create = 0
+		self.int_coun_create = 0
 		# ==========
 	# ----------------------------------------------------------------------------------
 	def make_test(self):
@@ -168,24 +169,34 @@ class Window(QWidget):
 		self.btn_next_create.show(), self.btn_back_create.show(), self.input_name_create.show(), self.input_coun_create.show()
 	
 	def create_next(self):
-		self.number_ind_create += 1
-		match self.number_ind_create:
+		self.int_ind_create += 1
+		match self.int_ind_create:
 			case 1:
-				self.input_name_create.hide(), self.input_coun_create.hide()
-			case self.number_coun_create:
+				self.int_coun_create = int(self.input_coun_create.text()) + 1
+				self.input_name_create.hide(), self.input_coun_create.hide(), self.btn_back_create.hide()
+				self.input_var1_create.show(), self.input_var2_create.show(), self.input_var3_create.show(), self.input_var4_create.show(),
+			case self.int_coun_create:
 				exit()
+			case _:
+				self.btn_back_create.show()
+				self.str_var1, self.str_var2, self.str_var3, self.str_var4 = \
+					self.input_var1_create.text(), self.input_var1_create.text(), \
+					self.input_var1_create.text(), self.input_var1_create.text()
+				self.cur.execute("INSERT INTO question(id, var1, var2, var3, var4)" +
+				                 f"VALUES ({self.int_ind_create - 1}, {self.str_var1}, {self.str_var2}, {self.str_var3}, {self.str_var4})")
+				self.input_var1_create.clear(), self.input_var2_create.clear(), self.input_var3_create.clear(), self.input_var4_create.clear(),
 	def create_back(self):
-		self.number_ind_create -= 1
-		match self.number_ind_create:
+		self.int_ind_create -= 1
+		match self.int_ind_create:
 			case -1:
-				self.number_ind_create = 0
+				self.int_ind_create = 0
 				self.main_window()
 				self.btn_next_create.hide(), self.btn_back_create.hide(), self.input_name_create.hide(), self.input_coun_create.hide()
 			case 0:
-				self.create_test()
-				
+				self.input_var1_create.hide(), self.input_var2_create.hide(), self.input_var3_create.hide(), self.input_var4_create.hide()
+			case 1:
+				self.btn_back_create.hide()
 			
-				
 if __name__ == '__main__':
 	pushButton_StyleSheet = '''
 	#pushButton {color: #000000; background-color: #4483e4; border: none; border-radius: 15px;}
